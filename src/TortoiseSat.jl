6c564046efdbf8165dcbd85d6f0a7af0707356f1
Andrew Gatherer <gatherer@stanford.edu>
2018-12-19 13:14:59 -0600

    retooled

--- src/TortoiseSat.jl
+++ src/TortoiseSat.jl
@@ -2,27 +2,22 @@
 
 using Pkg;
 using LinearAlgebra;
-Pkg.add("DifferentialEquations");
-using DifferentialEquations;
 using Plots;
 Pkg.add("TrajectoryOptimization");
-Pkg.add("SatProp")
-using TrajectoryOptimization
+using TrajectoryOptimization;
+Pkg.add("DifferentialEquations");
+using DifferentialEquations;
 
 
 #declare functions
-include("legendre.jl")
-include("dlegendre.jl")
 include("kep_cart.jl")
 include("cart_latlong.jl")
-include("igrf.jl")
-include("igrf12_coefs.jl")
-include("igrf12syn_coefs.jl")
 include("NED_cart.jl")
 include("DerivFunction.jl")
+include("OrbitPlotter.jl")
 
 #earth parameters
-GM = 3.986004418E14*(1/1000)^3; #earth graviational constant (km^3 s^-2)
+GM = 3.986004418E14*(1/1000)^3; #earth graviational constant (km^3 s^-2)""
 R_E = 6371.0; #earth radius (km)
 
 #satellite parameters
@@ -47,21 +42,9 @@ A[6] = 0; #initial true anomoly
 T=2*pi*sqrt(A[2].^3/GM);
 
 #time initialization
-t_int=0.0;
-t_fin=60.0*60*5; #seconds
-
-#initialize rotation vector
-
-#earth rotation
-omega_E=360/(23*60*60+56*60+4); #degree rotation per second
-
-# #initialize spice kernels
-# addpath('/Users/agatherer/Documents/Old_Documents/MATLAB/mice/src/mice/')
-# addpath('/Users/agatherer/Documents/Old_Documents/MATLAB/mice/lib/' )
-# cspice_furnsh('naif0012.tls');
-# cspice_furnsh('pck00010.tpc');
-# cspice_furnsh('de430.bsp');
-# et = cspice_str2et( '2019 JAN 31 01:00' ); %initial UTC time
+t0=0.0;
+tf=60.0*60*1.6; #seconds
+dt=0.1; #seconds
 
 #initialize vectors
 r_N_1=[0 0 -1];
@@ -91,35 +74,86 @@ eig_vects=eigvecs(K);
 eig_vect_0=eig_vects[:,location];
 q_N_B=eig_vect_0/norm(eig_vect_0);
 
-
 #initial
-pos_0=kep_cart(A,t_int,GM); #km
-att_int=[.2; 0; 0]; #rads
-omega_int=[.1; 0; 0]; #rads/s
-(lat_0,long_0)=cart_latlong(pos_0[1,1:3]);
-B_0=NED_cart(igrf12(2018,(alt+R_E)*1000,lat_0*pi/180,long_0*pi/180)*1E-9,lat_0,long_0);
+pos_0=kep_cart(A,t0,GM); #km and km/s
+att_int=[.2; .5; 0]; #rads
+omega_int=[.02; 0; 0]; #rads/s
+#find initial magnetic field
+B0=-8E15*(1E-3)^3;
 u0=[pos_0[1,:];pos_0[2,:];att_int;omega_int;q_N_B]; #km or km/s
 
-tspan=(t_int,t_fin);
-p=(J,GM,mass,m_max);
-prob=ODEProblem(DerivFunction!,u0,tspan,p);
-sol=solve(prob,reltol=1e-5,abstol=1e-5);
+#find magnetic field along 1 orbit
+tspan=(t0,tf);
+prob=DiffEqBase.ODEProblem(OrbitPlotter,u0,tspan);
+sol=DiffEqBase.solve(prob,dt=1.0,Euler())
+pos=sol[1:3,:]
+# plot(sol.t,pos[1,:])
+# plot!(sol.t,pos[2,:])
+# plot!(sol.t,pos[3,:])
 
-
-#plot satellite path
-plot3d(sol[1,:],sol[2,:],sol[3,:])
-
-#plot omega
-plot(sol.t,[sol[10,:] sol[11,:] sol[12,:]])
-
-#create lat and long vectors
-lat=zeros(size(sol,2),1);
-long=zeros(size(sol,2),1);
-for i=1:size(sol,2)
-    (lat_temp,long_temp)=cart_latlong(sol[1:3,i]);
-    lat[i]=lat_temp;
-    long[i]=rem(long_temp-omega_E*i*(t_fin-t_int)/length(sol.t)+180,360)+180;
+B_N=zeros(size(pos,2),3)
+for i=1:size(pos,2)
+    B_N[i,:]=[3*pos[1,i]*pos[3,i]*(B0/norm(pos[:,i])^5);3*pos[2,i]*pos[3,i]*(B0/norm(pos[:,i])^5);(3*pos[3,i]^2-norm(pos[:,i])^2)*(B0/norm(pos[:,i])^5)];
 end
+plot(sol.t,B_N[:,1])
+plot!(sol.t,B_N[:,2])
+plot!(sol.t,B_N[:,3])
+dt=(size(B_N,1)-1)/tf;
 
-#plot earth track
-plot(long,lat)
+#trajectory optimization section
+#initial
+omega_int=[.2;0;0];
+q_N_B=[0;0;0;0];
+x0=[omega_int;q_N_B;1];
+
+#final
+q_N_B_final=[0;0;0;0]
+omega_final=[0;0;0];
+xf=[omega_final;q_N_B_final;size(B_N,1)]; #km or km/s
+
+#bounds
+u_bnd=1;
+x_bnd=1;
+
+#create model
+n=8;
+m=3;
+model=Model(DerivFunction,n,m);
+
+#LQR
+Q = Array((1.e-9)*Diagonal(I,n));
+Qf = Array(1.e-9*Diagonal(I,n));
+R = Array((1.e-9)*Diagonal(I,m));
+
+#constant stage cost
+c=1.0
+
+#determine number of nodes
+N=size(B_N,1);
+
+obj = TrajectoryOptimization.UnconstrainedObjective(Q, R, Qf, c, tf, x0, xf);
+obj_con = ConstrainedObjective(obj,u_min=-u_bnd, u_max=u_bnd, x_min=-x_bnd, x_max=x_bnd);
+solver = TrajectoryOptimization.Solver(model,obj,N=N);
+solver.opts.verbose=true;
+solver.opts.use_static = false
+solver.opts.min_dt = dt
+solver.opts.max_dt = dt
+# solver.opts.iterations=5
+# solver.opts.iterations_outerloop=5
+# solver.opts.cost_intermediate_tolerance=1.0e-2;
+# solver.opts.cost_tolerance=1.0e-2;
+# solver.opts.gradient_intermediate_tolerance=1.0e-2
+# solver.opts.gradient_tolerance=1.0e-2
+# solver.opts.live_plotting=true
+
+
+U = zeros(m,solver.N);
+
+
+results, stats = TrajectoryOptimization.solve(solver,U)
+X = TrajectoryOptimization.to_array(results.X)
+plot(X[1,:])
+plot!(X[2,:])
+plot!(X[3,:])
+
+plot(X[8,:])
